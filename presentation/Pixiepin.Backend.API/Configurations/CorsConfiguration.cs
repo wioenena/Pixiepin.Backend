9a3690a7ec41e27fdfb13de3c50455221eead644
wioenena <wioenena@gmail.com>
2025-02-08 00:06:37 +0300

    Add CORS configuration class to backend API

---
+++ presentation/Pixiepin.Backend.API/Configurations/CorsConfiguration.cs
@@ -0,0 +1,7 @@
+namespace Pixiepin.Backend.API.Configurations;
+
+public static class CorsConfiguration {
+    public static IServiceCollection ApplyCorsConfiguration(this IServiceCollection services) {
+        return services;
+    }
+}
