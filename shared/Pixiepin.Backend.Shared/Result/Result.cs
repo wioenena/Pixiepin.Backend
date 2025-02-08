0c7b3a0a8b8cd806611443b65f095bc0a6331c6d
wioenena <wioenena@gmail.com>
2025-02-08 06:18:56 +0300

    Add Result class for handling operation outcomes with value and error types

---
+++ shared/Pixiepin.Backend.Shared/Result/Result.cs
@@ -0,0 +1,20 @@
+namespace Pixiepin.Backend.Shared.Result;
+
+public sealed class Result<TValue, TError> {
+    public TValue? Value { get; set; }
+    public TError? Error { get; set; }
+
+    internal Result(TValue value) {
+        this.Value = value;
+    }
+
+    internal Result(TError error) {
+        this.Error = error;
+    }
+}
+
+public static class Result {
+    public static Result<TValue, TError> Ok<TValue, TError>(TValue value) => new(value);
+
+    public static Result<TValue, TError> Fail<TValue, TError>(TError error) => new(error);
+}
