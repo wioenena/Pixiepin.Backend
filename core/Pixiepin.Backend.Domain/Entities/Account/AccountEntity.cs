faf9e57259a545b1d3be3b94efe059898e5f1771
wioenena <wioenena@gmail.com>
2025-02-08 06:17:44 +0300

    Add AccountEntity class to represent user account details

---
+++ core/Pixiepin.Backend.Domain/Entities/Account/AccountEntity.cs
@@ -0,0 +1,13 @@
+using Pixiepin.Backend.Domain.Entities.Common;
+
+namespace Pixiepin.Backend.Domain.Entities.Account;
+
+public sealed class AccountEntity : BaseEntity {
+    public required string Username { get; set; }
+    public string? Email { get; set; }
+    public required string Password { get; set; }
+    public DateTime? LastLoginAt { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string CompanyName { get; set; }
+}
