c6d3892eef4c7a3bbe572b28400dcf57f618b893
wioenena <wioenena@gmail.com>
2025-02-08 06:18:40 +0300

    Add AuthController for handling authentication-related API requests

---
+++ presentation/Pixiepin.Backend.API/Controllers/AuthController.cs
@@ -0,0 +1,9 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pixiepin.Backend.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase {
+}
