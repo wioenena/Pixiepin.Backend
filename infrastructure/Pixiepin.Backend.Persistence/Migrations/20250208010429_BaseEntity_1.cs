4ce90d75849d58f6a13881f5facac67581e76a33
wioenena <wioenena@gmail.com>
2025-02-08 06:19:02 +0300

    Add migration for Accounts table with necessary fields and constraints

---
+++ infrastructure/Pixiepin.Backend.Persistence/Migrations/20250208010429_BaseEntity_1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pixiepin.Backend.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class BaseEntity_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    username = table.Column<string>(type: "varchar(20)", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    lastLoginAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    firstName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    lastName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    companyName = table.Column<string>(type: "varchar(100)", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    isDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_email",
+                table: "Accounts",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_username",
+                table: "Accounts",
+                column: "username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
