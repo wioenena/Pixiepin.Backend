fa19b0d022149aaf1fbb5e9e69582ad1f523ee9f
wioenena <wioenena@gmail.com>
2025-02-08 06:18:10 +0300

    Add AccountConfiguration class for configuring AccountEntity properties and indexes

---
+++ infrastructure/Pixiepin.Backend.Persistence/Configurations/AccountConfiguration.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Pixiepin.Backend.Domain.Entities.Account;
+
+namespace Pixiepin.Backend.Persistence.Configurations;
+
+public sealed class AccountConfiguration : IEntityTypeConfiguration<AccountEntity> {
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<AccountEntity> builder) {
+        _ = builder.ToTable("Accounts");
+        _ = builder
+                .HasIndex(a => a.Email)
+                .IsUnique();
+        _ = builder
+                .HasIndex(a => a.Username)
+                .IsUnique();
+        _ = builder
+                .Property(a => a.Email)
+                .IsRequired(false)
+                .HasColumnName("email");
+        _ = builder
+                .Property(a => a.Username)
+                .IsRequired(true)
+                .HasColumnName("username")
+                .HasColumnType("varchar(20)");
+        _ = builder
+                .Property(a => a.Password)
+                .IsRequired(true)
+                .HasColumnName("password");
+        _ = builder.Property(a => a.LastLoginAt)
+                .IsRequired(false)
+                .HasColumnName("lastLoginAt");
+        _ = builder.Property(a => a.FirstName)
+                .IsRequired(true)
+                .HasColumnName("firstName")
+                .HasColumnType("varchar(50)");
+        _ = builder.Property(a => a.LastName)
+                .IsRequired(true)
+                .HasColumnName("lastName")
+                .HasColumnType("varchar(50)");
+        _ = builder.Property(a => a.CompanyName)
+                .IsRequired(true)
+                .HasColumnName("companyName")
+                .HasColumnType("varchar(100)");
+    }
+}
