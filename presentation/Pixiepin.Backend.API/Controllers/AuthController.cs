67983f09d466cc76aa4f416150e13bbf078cca2d
wioenena <wioenena@gmail.com>
2025-02-20 10:27:35 +0300

    Refactor AuthController to use MediatR for account registration and login

--- presentation/Pixiepin.Backend.API/Controllers/AuthController.cs
+++ presentation/Pixiepin.Backend.API/Controllers/AuthController.cs
@@ -1,9 +1,19 @@
-using System.Diagnostics;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Pixiepin.Backend.Application.Features.Auth.CreateAccount;
+using Pixiepin.Backend.Application.Features.Auth.Login;
 
 namespace Pixiepin.Backend.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AuthController : ControllerBase {
+public class AuthController(IMediator mediator) : ControllerBase {
+    private readonly IMediator mediator = mediator;
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(CreateAccountRequest request) => this.Ok(await this.mediator.Send(request));
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginRequest request) => this.Ok(await this.mediator.Send(request));
 }
