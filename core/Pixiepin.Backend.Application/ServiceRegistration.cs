37dc99985c104d87d9cadebeec780b9d96405264
wioenena <wioenena@gmail.com>
2025-02-20 10:22:50 +0300

    Enhance service registration to include FluentValidation and validation behavior

--- core/Pixiepin.Backend.Application/ServiceRegistration.cs
+++ core/Pixiepin.Backend.Application/ServiceRegistration.cs
@@ -1,12 +1,19 @@
 using System.Reflection;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
+using Pixiepin.Backend.Application.Behaviors;
 
 namespace Pixiepin.Backend.Application;
 
 public static class ServiceRegistration {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services) {
         var assembly = Assembly.GetExecutingAssembly();
-        _ = services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+        _ = services.AddMediatR(config => {
+            _ = config.RegisterServicesFromAssembly(assembly);
+            _ = config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        _ = services.AddValidatorsFromAssembly(assembly);
         return services;
     }
 }
