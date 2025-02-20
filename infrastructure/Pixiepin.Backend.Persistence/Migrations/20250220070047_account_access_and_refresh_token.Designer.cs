32a3b86ee63958684d7de0d21ac5a2f916aa7800
wioenena <wioenena@gmail.com>
2025-02-20 10:26:40 +0300

    Add AccessToken and RefreshToken columns to Accounts table in migration

--- infrastructure/Pixiepin.Backend.Persistence/Migrations/20250220070047_account_access_and_refresh_token.Designer.cs
+++ infrastructure/Pixiepin.Backend.Persistence/Migrations/20250220070047_account_access_and_refresh_token.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Pixiepin.Backend.Persistence.Contexts;
@@ -11,9 +12,11 @@ using Pixiepin.Backend.Persistence.Contexts;
 namespace Pixiepin.Backend.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250220070047_account_access_and_refresh_token")]
+    partial class account_access_and_refresh_token
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ infrastructure/Pixiepin.Backend.Persistence/Migrations/20250220070047_account_access_and_refresh_token.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pixiepin.Backend.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class account_access_and_refresh_token : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "accessToken",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "refreshToken",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "accessToken",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "refreshToken",
+                table: "Accounts");
+        }
+    }
+}
