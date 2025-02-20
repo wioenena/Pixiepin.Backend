417bf161d06176d2f834cb5d00c6fb7dd8dd07c8
wioenena <wioenena@gmail.com>
2025-02-20 10:27:13 +0300

    Add JWT and Password services to persistence layer and configure JWT settings

--- infrastructure/Pixiepin.Backend.Persistence/ServiceRegistration.cs
+++ infrastructure/Pixiepin.Backend.Persistence/ServiceRegistration.cs
@@ -1,13 +1,20 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Pixiepin.Backend.Application.Interfaces;
+using Pixiepin.Backend.Application.Interfaces.Services;
 using Pixiepin.Backend.Persistence.Contexts;
+using Pixiepin.Backend.Persistence.Services;
+using Pixiepin.Backend.Persistence.Settings;
 
 namespace Pixiepin.Backend.Persistence;
 
 public static class ServiceRegistration {
-    public static IServiceCollection AddPersistenceServices(this IServiceCollection services) {
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration) {
         _ = services.AddDbContext<ApplicationDbContext>();
         _ = services.AddScoped<IUnitOfWork, UnitOfWork>();
+        _ = services.AddSingleton<IPasswordService, PasswordService>();
+        _ = services.AddSingleton<IJWTService, JWTService>();
+        _ = services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
         return services;
     }
 }
